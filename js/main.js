933077dd6d689e5d460525be3193557282c90b99
weipeng <weipeng@8fc169b5-ef5d-41da-9b5a-8861938b1d8c>
2015-02-19 21:45:05 +0000

    fixes

--- js/main.js
+++ js/main.js
@@ -148,8 +148,7 @@ $(document).ready(function(){
 		$('section.devices .section-graphic').css('bottom', '400px');
 		$('section.devices .section-graphic').css('left', '50%');
 		$('section.devices .section-graphic').css('margin-left', '-270px');
-	}
-	if($(window).width() < 1281 && $(window).width() >= 1001 && $(window).height > 730){
+	}else if($(window).width() < 1281 && $(window).width() >= 1001 && $(window).height > 730){
 		$('section.devices .section-graphic').css('position', 'absolute');
 		$('section.devices .section-graphic').css('bottom', '400px');
 		$('section.devices .section-graphic').css('left', '50%');
@@ -161,10 +160,7 @@ $(document).ready(function(){
 			$('section.devices .section-graphic').css('bottom', '400px');
 			$('section.devices .section-graphic').css('left', '50%');
 			$('section.devices .section-graphic').css('margin-left', '-270px');
-		}else{
-			$('section.devices .section-graphic').removeAttr("style");
-		}
-		if($(window).width() < 1281 && $(window).width() >= 1001 && $(window).height > 730){
+		}else if($(window).width() < 1281 && $(window).width() >= 1001 && $(window).height > 730){
 			$('section.devices .section-graphic').css('position', 'absolute');
 			$('section.devices .section-graphic').css('bottom', '400px');
 			$('section.devices .section-graphic').css('left', '50%');
